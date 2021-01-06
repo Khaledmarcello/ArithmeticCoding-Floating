4e8f3d9d18e5c8e9c0d5b76f61d96201de8a84aa
Khaledmarcello <66645843+Khaledmarcello@users.noreply.github.com>
2021-01-06 04:41:10 +0200

    Modification about paths

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String args[]) {
         ArithmeticCoding a=new ArithmeticCoding();
-        a.Compress("C:\\Users\\RCSC\\Desktop\\ArithmeticCoding-Floating\\test.txt", "compressed");
-        a.Decompress("C:\\Users\\RCSC\\Desktop\\ArithmeticCoding-Floating\\compressed.txt", "decompressed");
+        a.Compress("C:\\Users\\RCSC\\Documents\\GitHub\\ArithmeticCoding-Floating\\test.txt", "compressed");
+        a.Decompress("C:\\Users\\RCSC\\Documents\\GitHub\\ArithmeticCoding-Floating\\compressed.txt", "decompressed");
     }
 }
